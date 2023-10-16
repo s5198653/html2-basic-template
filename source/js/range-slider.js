dc3509904c33ab241af2a09f02b7dbed50158bbc
Taranovskaya <s5198653@yandex.ru>
2023-10-16 22:27:09 +0300

    Исправляет работу range slider

--- source/js/range-slider.js
+++ source/js/range-slider.js
@@ -9,6 +9,21 @@ const rangeElement = document.querySelector('.range__slider');
 const minInput = document.querySelector('.range__input--min');
 const maxInput = document.querySelector('.range__input--max');
 
+const setInputValue = () => {
+  const value = rangeElement.noUiSlider.get();
+  minInput.value = Math.floor(value[0]);
+  maxInput.value = Math.floor(value[1]);
+};
+
+const addRangeUpdatingListner = () => {
+  rangeElement.noUiSlider.on('update', setInputValue);
+};
+
+const addInputsUpdatingListners = () => {
+  minInput.addEventListener('input', ({target: {value}}) => rangeElement.noUiSlider.set([value, null]));
+  maxInput.addEventListener('input', ({target: {value}}) => rangeElement.noUiSlider.set([null, value]));
+};
+
 const initRange = () => {
   noUiSlider.create(rangeElement, {
     start: [DEFAULT__VALUES.start, DEFAULT__VALUES.end],
@@ -19,16 +34,8 @@ const initRange = () => {
     },
     step: DEFAULT__VALUES.step
   });
+  addRangeUpdatingListner();
+  addInputsUpdatingListners();
 };
 
-const setInputValue = () => {
-  const value = rangeElement.noUiSlider.get();
-  minInput.value = Math.floor(value[0]);
-  maxInput.value = Math.floor(value[1]);
-};
-
-const updateRange = () => {
-  rangeElement.noUiSlider.on('update', setInputValue);
-};
-
-export {initRange, updateRange};
+export {initRange};
--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,7 @@
 /* в этот файл добавляет скрипты*/
 import './burger-menu.js';
-import { initRange, updateRange } from './range-slider.js';
+import { initRange } from './range-slider.js';
 import { initSlider } from './slider.js';
 
 initRange();
-updateRange();
 initSlider();
